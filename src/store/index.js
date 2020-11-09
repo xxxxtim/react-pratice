69913cb99b5e4d5c2d53c2bce7a0742083681277
xxxxtim <xxxxtim@hotmail.com>
2020-11-09 16:50:41 +0800

    feat: install reactThunk | apply react devTool

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,24 @@
-import { createStore } from 'redux';
-// 引入reducer.js
+import { createStore, applyMiddleware, compose } from 'redux';// 引入reducer.js 和redux
 import reducer from './reducer'
-const store = createStore(
-    reducer,
-    // redux套件要增加下面一行
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+import thunk from 'redux-thunk';
+// const store = createStore(
+//     reducer,
+//     applyMiddleware(thunk),
+//     // redux套件要增加下面一行
+//     // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
+
+
+const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+            // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+        }) : compose;
+
+const enhancer = composeEnhancers(
+    applyMiddleware(thunk),
+    // other store enhancers if any
 );
+const store = createStore(reducer, enhancer);
 
 export default store;
